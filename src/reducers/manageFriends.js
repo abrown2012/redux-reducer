f1a7bcca8e694608fb1c30409653721ebc77d866
Agnieszka Brown <abrown2012@icloud.com>
2021-07-29 15:01:53 -0400

    commit tree d0e04ed0992deee0568297a49550ebfec999bef6

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+
+    switch (action.type) {
+          case 'ADD_FRIEND':
+            return {...state, friends: [
+                ...state.friends, action.friend
+            ]}
+          case 'REMOVE_FRIEND':
+            return {...state, friends: [
+                ...state.friends.slice(0, state.friends.findIndex(friend => friend.id===action.id)), 
+                ...state.friends.slice(state.friends.findIndex(friend => friend.id===action.id) +1)
+            ]}
+          default:
+            return state;
+        }
 }
